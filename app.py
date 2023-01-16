776a016a7ddb6095669e4d73f2ae9b14f40abda7
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2023-01-15 21:13:32 -0600

    added new branch for reducing combinations

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import json
 from flask import Flask, render_template, request, jsonify, url_for, redirect, \
-    flash
+    flash, session
 from controller import get_initial_panels, get_filtered_panels
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/data', methods=["GET"])
+def get_data():
+    offset = request.args.get('offset', 0, type=int)
+    limit = request.args.get('limit', 12, type=int)
+    return get_partial_data(offset, limit)
+
+
+def get_partial_data(offset, limit):
+    return session['panels'][offset:offset + limit]
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -12,19 +23,22 @@ def index():
     if request.method == "POST":
         try:
             if "primary_form" in request.form:
-                # flash("Primary form submitted", "success")
+                # CD103,CD11c,CD138,CD5,CD69,CD8a,CD73,CD38
                 antibody = request.form["antibody"].strip()
                 antibodies = [s.strip() for s in antibody.split(",")]
                 panels, not_found = get_initial_panels(antibodies)
+                session['panels'] = panels
+                total_panels = len(panels)
+                panels_chunked = get_partial_data(0, 12)
                 for item in not_found:
                     flash(f"Antibody <b>{item}</b> not found", "warning")
-                return render_template('index.html', panels=panels,
+                return render_template('index.html',
+                                       total_panels=total_panels,
+                                       panels=panels_chunked,
                                        old_patterns="",
                                        searched_antibody=antibody)
             elif "secondary_form" in request.form:
-                # flash("Secondary form submitted", "success")
-                panels = json.loads(
-                    request.form["old_panels"].replace("\'", "\""))
+                panels = session.get('panels', [])
                 old_patterns = request.form["old_patterns"].strip()
                 patterns = request.form["patterns"].strip()
                 searched_antibody = request.form["searched_antibody"].strip()
@@ -38,9 +52,13 @@ def index():
                     old_patterns = patterns
                 panels, not_found_patterns = get_filtered_panels(panels,
                                                                  patterns)
+                session['panels'] = panels
+                total_panels = len(panels)
+                panels_chunked = get_partial_data(0, 12)
                 for item in not_found_patterns:
                     flash(f"Pattern <b>{item}</b> not found", "warning")
-                return render_template('index.html', panels=panels,
+                return render_template('index.html', total_panels=total_panels,
+                                       panels=panels_chunked,
                                        old_patterns=old_patterns,
                                        searched_antibody=searched_antibody)
         except Exception as ex:
